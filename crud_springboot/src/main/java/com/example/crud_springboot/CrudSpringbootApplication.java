45b94c5a5100fe67faf94aa3b6c57880622135a8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:44:28 -0500

    Creación Proyecto

---
+++ crud_springboot/src/main/java/com/example/crud_springboot/CrudSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbootApplication.class, args);
+	}
+
+}
---
+++ crud_springboot/src/test/java/com/example/crud_springboot/CrudSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
