6a9a55f0fe2e284b3435093f5ee052a5470c17c2
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:46:31 -0500

    Create UserModel.java

---
+++ crud_springboot/src/main/java/com/example/crud_springboot/model/UserModel.java
@@ -0,0 +1,10 @@
+package com.example.crud_springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_usuario")
+public class UserModel {
+    
+}
