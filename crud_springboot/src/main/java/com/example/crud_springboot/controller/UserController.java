25808f2391617af37030306ba43deca3fceb8e40
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:53:16 -0500

    Update UserController.java

--- crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.crud_springboot.controller;
 
+import com.example.crud_springboot.model.UserModel;
 import com.example.crud_springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/user")
@@ -11,5 +15,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    /*Definir peticiones http*/
+    @GetMapping
+    public ArrayList<UserModel> getUsers(){
+        return this.userService.getUsers();
+    }
 
 }
