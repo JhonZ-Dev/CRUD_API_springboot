b914e3954c8be1a08e2f03b07cfc6a85bbea98b5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:47:09 -0500

    Update UserModel.java

--- crud_springboot/src/main/java/com/example/crud_springboot/model/UserModel.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/model/UserModel.java
@@ -1,10 +1,22 @@
 package com.example.crud_springboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tbl_usuario")
+@Data
+@NoArgsConstructor
+
 public class UserModel {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String email;
 }
