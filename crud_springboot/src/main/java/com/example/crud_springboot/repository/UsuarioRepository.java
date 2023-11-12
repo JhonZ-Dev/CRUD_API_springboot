cd12746f7ed583c22a5c2faa8f4c99c1e505d9a8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:48:02 -0500

    Update UsuarioRepository.java

--- crud_springboot/src/main/java/com/example/crud_springboot/repository/UsuarioRepository.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/repository/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package com.example.crud_springboot.repository;
 
+import com.example.crud_springboot.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<UserModel, Long> {
 }
