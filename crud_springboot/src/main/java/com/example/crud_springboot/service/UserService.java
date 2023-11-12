9b7b031c952111a0e8ba57edb94eaa9005691671
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:50:06 -0500

    Update UserService.java

--- crud_springboot/src/main/java/com/example/crud_springboot/service/UserService.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.crud_springboot.service;
 
+import com.example.crud_springboot.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    
+    /*Inyecciones de depencias con AutoWire*/
+    @Autowired
+    public UsuarioRepository usuarioRepository;
 }
