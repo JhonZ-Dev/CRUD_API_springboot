bb932595f441f993e5d97a9d39dbcbb951f43ec9
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:53:45 -0500

    Update UserController.java

--- crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +24,11 @@ public class UserController {
     public UserModel saveUser(@RequestBody UserModel userModel){
 
         return this.userService.saveUser(userModel);
+    }
+    /*Traer el usuario por id*/
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Integer id){
+        return this.userService.getById(id);
     }
 
 }
