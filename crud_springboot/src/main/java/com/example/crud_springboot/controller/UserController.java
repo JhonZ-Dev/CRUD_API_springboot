2aed14d82509d064c60bd8e90cdac52d4517a532
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-11 22:53:30 -0500

    Update UserController.java

--- crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
+++ crud_springboot/src/main/java/com/example/crud_springboot/controller/UserController.java
@@ -3,9 +3,7 @@ package com.example.crud_springboot.controller;
 import com.example.crud_springboot.model.UserModel;
 import com.example.crud_springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -20,6 +18,11 @@ public class UserController {
     @GetMapping
     public ArrayList<UserModel> getUsers(){
         return this.userService.getUsers();
+    }
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel userModel){
+
+        return this.userService.saveUser(userModel);
     }
 
 }
